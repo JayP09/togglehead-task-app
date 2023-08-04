00972290e6d0de70f33551a7556efc583f9ca49f
Jay <panchalj272@gmail.com>
2023-08-04 18:08:15 +0530

    feat: Responsive Stats Section

---
+++ src/components/StatsSection/StatsSection.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./StatsSection.css";
+
+const StatsSection = () => {
+  return (
+    <div className="statsContainer">
+      <div className="statsItems">
+        <h3 className="title">Lorem ipsum</h3>
+        <p className="stats">123+</p>
+        <h3>Lorem ipsum</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+        </p>
+      </div>
+      <div className="statsItems">
+        <h3 className="title">Lorem ipsum</h3>
+        <p className="stats">12+</p>
+        <h3>Lorem ipsum</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+        </p>
+      </div>
+      <div className="statsItems">
+        <h3 className="title">Lorem ipsum</h3>
+        <p className="stats">12+</p>
+        <h3>Lorem ipsum</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et
+        </p>
+      </div>
+      <div className="statsItems">
+        <h3 className="title">Lorem ipsum</h3>
+        <p className="stats">123+</p>
+        <h3>Lorem ipsum</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default StatsSection;
