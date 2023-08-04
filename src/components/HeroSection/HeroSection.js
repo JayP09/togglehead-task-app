2f6b64688ac736b4f0f34981752677ecef332a30
Jay <panchalj272@gmail.com>
2023-08-04 18:07:56 +0530

    feat: Responsive Herosection

---
+++ src/components/HeroSection/HeroSection.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./HeroSection.css";
+
+const HeroSection = () => {
+  return (
+    <main className="container">
+      <div className="containerLeft">
+        <h1>Lorem ipsum</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd
+        </p>
+        <button>
+          Know more
+          <span>
+            <i class="fa-solid fa-caret-right"></i>
+          </span>
+        </button>
+      </div>
+      <div className="containerRight">
+        <img src="/herosection.jpg" alt="hero section" />
+      </div>
+    </main>
+  );
+};
+
+export default HeroSection;
