f0e5df4ad19e36fbcb66d3af4a8eb269fdafa870
Jay <panchalj272@gmail.com>
2023-08-04 18:10:56 +0530

    feat: Updated File with Herosection, Navbar, StatsSection, SubscribeSection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import HeroSection from "./components/HeroSection/HeroSection";
+import Navbar from "./components/Navbar/Navbar";
+import StatsSection from "./components/StatsSection/StatsSection";
+import SubscribeSection from "./components/SubscribeSection/SubscribeSection";
 
 function App() {
-  return <div className="App">toggleHead Task App</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <HeroSection />
+      <StatsSection />
+      <SubscribeSection />
+    </div>
+  );
 }
 
 export default App;
